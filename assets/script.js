fd681b0dd00681786894acf025128a3bba897813
Kilimba <dyaul003@umn.edu>
2022-06-12 03:05:18 -0500

    got my containers spaced out better

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,17 @@
 var searchFormEl = document.querySelector("#search-form");
-var cityInputEl = document.querySelector("#city");
-var cityContainerEl = document.querySelector("#city-container");
-var citySearchTerm = document.querySelector("#city-search-term");
+var cityNameInputEl = document.querySelector("#city");
+var forecastEl = document.querySelector("#forecast");
+var cityContainerEl = document.querySelector("#temp");
+
 
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var city = cityInputEl.value.trim();
+    var citySearch = cityNameInputEl.value.trim();
 
-    if(city){
-        getWeatherData(city);
-        cityInputEl.value = "";
+    if(citySearch){
+        getWeatherData(citySearch);
+        cityNameInputEl.value = "";
     } else {
         alert("Please enter a valid city name!");
     }
@@ -19,57 +20,40 @@ var formSubmitHandler = function(event) {
 
 // allows me to get the weather of one city right now (used the geo one)
 // API key: 23e37cb222f3e68135a0aa3f62649568
-const getWeatherData = function (city) {
-    let apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=23e37cb222f3e68135a0aa3f62649568';
-
+var getWeatherData = function (city) {
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&limit=5' + '&appid=23e37cb222f3e68135a0aa3f62649568' + '&units=imperial' +'&cnt=5';
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             // displayCity(data, city);
             console.log(data);
         });
     });
-};
+}
 
-getWeatherData('london');
+var displayCity  = function (cities) {
+    cityContainerEl.textContent = '';
+// loop over repos
+for (var i = 0; i < cities.length; i++) {
+    // format (I still NEED uv index)
+    var cityForecast = cities[i].city.name + cities[i].list.main.temp;
+    
+    //  + cities[i].list.main.temp.imperial + cities[i].list.wind.speed + cities[i].list.main.humidity + cities[i].list.weather.icon;
+  
+  
 
-// var displayCity = function (city, searchTerm) {
-//     cityContainerEl.textContent = "";
-//     citySearchTerm.textContent = searchTerm;
-//     // loop over repos
-// for (var i = 0; i < city.length; i++) {
-//     // format repo name (I still NEED uv index)
-//     var cityName = city[i].name + city[i].main.temp + city[i].wind.speed + city[i].main.humidity;
+    // create a <p> for each data point 
+    var cityEl = $('<div>')
+        .addClass('#temp')
+        .text(cityForecast)
+        // .attr({
+        //     id: "temp"
+        // })
   
-//     // create a container for each city
-//     var cityEl = document.createElement("div");
+    // append to container
+    forecastEl.appendChild(cityEl);
+  }
+}
 
-  
-//     // create a span element to hold repository name
-//     var titleEl = document.createElement("span");
-//     titleEl.textContent = cityName;
-  
-//     // append to container
-//     cityEl.appendChild(titleEl);
-  
-//     // append container to the dom
-//     cityContainerEl.appendChild(cityEl);
-//   }
-// }
-
-
-// var displayWeatherForecast = function (city, searchTerm) {
-//     cityContainerEl.textContent = "";
-//     citySearchTerm.textContent = searchTerm;
-//     // loop over repos
-// for (var i = 0; i < city.length; i++) {
-//     // format repo name (I still NEED uv index)
-//     var cityForecast = city[i].name +
-  
-  
-//     // append container to the dom
-//     cityContainerEl.appendChild(cityEl);
-//   }
-// }
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
 
