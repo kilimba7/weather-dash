81a1b9cdaa34b34f0ad913e42062aba13c392a07
Kilimba <dyaul003@umn.edu>
2022-06-12 17:23:13 -0500

    got my 5 day forecast to work. Now I need the daily one

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,65 @@
 var searchFormEl = document.querySelector("#search-form");
-var cityNameInputEl = document.querySelector("#city");
-var forecastEl = document.querySelector("#forecast");
-var cityContainerEl = document.querySelector("#temp");
-
-
+var searchInputEl = document.querySelector("#city");
+var forecast = $("#forecast");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var citySearch = cityNameInputEl.value.trim();
+    var citySearch = searchInputEl.value.trim();
 
     if(citySearch){
-        getWeatherData(citySearch);
-        cityNameInputEl.value = "";
+        getWeatherForecast(citySearch);
+        searchInputEl.value = "";
     } else {
         alert("Please enter a valid city name!");
     }
     console.log(event);
   };
 
-// allows me to get the weather of one city right now (used the geo one)
-// API key: 23e37cb222f3e68135a0aa3f62649568
-var getWeatherData = function (city) {
-    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&limit=5' + '&appid=23e37cb222f3e68135a0aa3f62649568' + '&units=imperial' +'&cnt=5';
+// API key: f48f74826f6fb834b97afa06980a605e
+var getWeatherForecast = function (citySearch) {
+    console.log(citySearch);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=f48f74826f6fb834b97afa06980a605e&units=imperial"
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            // displayCity(data, city);
             console.log(data);
+
+      //loop to create a new card for 5 days
+      for (var i = 0; i < data.list.length; i++) {
+
+        if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+          var title = $("<h3>")
+          .addClass("card-title")
+          .text(new Date(data.list[i].dt_txt)
+          .toLocaleDateString());
+          
+          var img = $("<img>")
+          .attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+          
+          var col = $("<div>")
+          .addClass("col-sm");
+          
+          var card = $("<div>")
+          .addClass("card bg-primary text-white");
+          
+          var cardBody = $("<div>")
+          .addClass("card-body p-2");
+          
+          var humid = $("<p>")
+          .addClass("card-text")
+          .text("Humidity: " + data.list[i].main.humidity + "%");
+          
+          var temp = $("<p>")
+          .addClass("card-text")
+          .text("Temperature: " + data.list[i].main.temp + " °F");
+
+          // Append everything to display
+          col.append(card.append(cardBody.append(title, img, temp, humid)))
+          forecast.append(col);
+        }
+      }
         });
     });
-}
-
-var displayCity  = function (cities) {
-    cityContainerEl.textContent = '';
-// loop over repos
-for (var i = 0; i < cities.length; i++) {
-    // format (I still NEED uv index)
-    var cityForecast = cities[i].city.name + cities[i].list.main.temp;
-    
-    //  + cities[i].list.main.temp.imperial + cities[i].list.wind.speed + cities[i].list.main.humidity + cities[i].list.weather.icon;
-  
-  
-
-    // create a <p> for each data point 
-    var cityEl = $('<div>')
-        .addClass('#temp')
-        .text(cityForecast)
-        // .attr({
-        //     id: "temp"
-        // })
-  
-    // append to container
-    forecastEl.appendChild(cityEl);
-  }
 }
 
 
