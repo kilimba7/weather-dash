f0900bb5ab5d337a72fbda9016ff5dc3f5c1b399
Kilimba <dyaul003@umn.edu>
2022-06-13 18:07:58 -0500

    got my daily forecast to show up

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,87 @@
 var searchFormEl = document.querySelector("#search-form");
 var searchInputEl = document.querySelector("#city");
 var forecast = $("#forecast");
+var cityHeader = $("#city-header");
+var dailyForecast = $("#daily-forecast");
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    var citySearch = searchInputEl.value.trim();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var citySearch = searchInputEl.value.trim();
 
-    if(citySearch){
-        getWeatherForecast(citySearch);
-        searchInputEl.value = "";
-    } else {
-        alert("Please enter a valid city name!");
-    }
-    console.log(event);
-  };
+  if (citySearch) {
+     getForecastData(citySearch);
+     searchInputEl.value = "";
+  } else {
+     alert("Please enter a valid city name!");
+  }
+  console.log(event);
+};
+
+var getForecastData = function (citySearch) {
+    var cityName = '';
+    // API key: f48f74826f6fb834b97afa06980a605e
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=f48f74826f6fb834b97afa06980a605e&units=imperial"
+    fetch(apiUrl).then(function (response) {
+       response.json().then(function (data) {
+          console.log(data);
+          getDailyWeatherForecast(data);
+          getFiveDayWeatherForecast(data);
+ 
+          cityName = $("<h2>")
+             .addClass("pr-3 pb-2 card")
+             .text("Daily Forecast for: " + data.city.name);
+
+        
+          cityHeader.append(cityName);
+       });
+    });
+ 
+ }
+ 
+ var getDailyWeatherForecast = function (forecastData) {
+
+ 
+       // only for todays forecast records
+       if (forecastData.list[0]) {
+ 
+        
+          var humid = $("<p>")
+             .addClass("card-text")
+             .text("Humidity: " + forecastData.list[0].main.humidity + "%");
+ 
+          var wind = $("<p>")
+             .addClass("card-text")
+             .text("Wind Speed: " + forecastData.list[0].wind.speed + " MPH");
+ 
+          var temp = $("<p>")
+             .addClass("card-text")
+             .text("Temp: " + forecastData.list[0].main.temp + " °F");
+       
+       // Append everything to display
+    
+      dailyForecast.append(temp, wind, humid);
+     }
+    
+  }
+    
+
+ 
 
 // API key: f48f74826f6fb834b97afa06980a605e
-var getWeatherForecast = function (citySearch) {
-    console.log(citySearch);
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=f48f74826f6fb834b97afa06980a605e&units=imperial"
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+var getFiveDayWeatherForecast = function (citySearch) {
 
       //loop to create a new card for 5 days
-      for (var i = 0; i < data.list.length; i++) {
+      for (var i = 0; i < citySearch.list.length; i++) {
 
-        if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+        if (citySearch.list[i].dt_txt.indexOf("15:00:00") !== -1) {
 
           var title = $("<h3>")
           .addClass("card-title")
-          .text(new Date(data.list[i].dt_txt)
+          .text(new Date(citySearch.list[i].dt_txt)
           .toLocaleDateString());
           
           var img = $("<img>")
-          .attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+          .attr("src", "https://openweathermap.org/img/w/" + citySearch.list[i].weather[0].icon + ".png");
           
           var col = $("<div>")
           .addClass("col-sm");
@@ -47,23 +94,21 @@ var getWeatherForecast = function (citySearch) {
           
           var humid = $("<p>")
           .addClass("card-text")
-          .text("Humidity: " + data.list[i].main.humidity + "%");
+          .text("Humidity: " + citySearch.list[i].main.humidity + "%");
           
           var temp = $("<p>")
           .addClass("card-text")
-          .text("Temp: " + data.list[i].main.temp + " °F");
+          .text("Temp: " + citySearch.list[i].main.temp + " °F");
 
           var wind = $("<p>")
           .addClass("card-text")
-          .text("Wind Speed: " + data.list[i].wind.speed + " MPH");
+          .text("Wind Speed: " + citySearch.list[i].wind.speed + " MPH");
 
           // Append everything to display
           col.append(card.append(cardBody.append(title, img, temp, wind, humid)))
           forecast.append(col);
         }
       }
-        });
-    });
 }
 
 
